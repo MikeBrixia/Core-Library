5675094f6e9c1da63252b790d3f7354548a82abc
MikeBrixia <michael.guida@outlook.com>
2022-05-10 13:19:56 +0200

    Prepared package
    Now core library package is recognized by Unity package manager

--- Runtime/Components/MovementComponent2D.cs
+++ Runtime/Components/MovementComponent2D.cs
@@ -1,6 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 using UnityEngine.Events;
 
 namespace Core
@@ -563,4 +567,3 @@ namespace Core
         }
     }
 }
-
