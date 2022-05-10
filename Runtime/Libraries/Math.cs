df9adaa5ffe354d37b354493c4eabb1bcac14301
MikeBrixia <michael.guida@outlook.com>
2022-05-10 13:28:19 +0200

    Updated docs
    Changed math lib docs

--- Runtime/Libraries/Math.cs
+++ Runtime/Libraries/Math.cs
@@ -9,32 +9,32 @@ namespace Core
     /// <summary>
     /// Get parable point at given time.
     /// </summary>
-    /// <param name="InitialLocation">The location at which the parable is gonna start.</param>
+    /// <param name="initialLocation">The location at which the parable is gonna start.</param>
     /// <returns>The parable point at given time.</returns>
-    public static Vector2 GetParableTrajectory(Vector2 InitialLocation, Vector2 InitialVelocity, float Time)
+    public static Vector2 GetParableTrajectory(Vector2 initialLocation, Vector2 initialVelocity, float time)
     {
-      return InitialLocation + InitialVelocity * Time + 0.5f * Physics2D.gravity * Mathf.Pow(Time, 2);
+      return initialLocation + initialVelocity * time + 0.5f * Physics2D.gravity * Mathf.Pow(time, 2);
     }
   
     /// <summary>
     /// Get the time at which the parable it's gonna reach the maximum height.
     /// </summary>
-    /// <param name="Velocity">The velocity of the body.</param>
-    /// <returns>The parable point at given time.</returns>
-    public static float GetParableMaxHeightTime(Vector2 Velocity, Vector2 Gravity)
+    /// <param name="velocity">The velocity of the body.</param>
+    /// <returns>Max height time of the parable.</returns>
+    public static float GetParableMaxHeightTime(Vector2 velocity, Vector2 gravity)
     {
-      return Mathf.Abs(Velocity.y / Gravity.y);
+      return Mathf.Abs(velocity.y / gravity.y);
     }
   
     /// <summary>
     /// Get the unit direction between two points.
     /// </summary>
-    /// <param name="From">The first point.</param>
-    /// <param name="From">The second point.</param>
-    /// <returns>direction between these two points normalized.</returns>
-    public static Vector2 GetUnitDirectionVector(Vector2 From, Vector2 To)
+    /// <param name="from">The starting point.</param>
+    /// <param name="to">The end point.</param>
+    /// <returns>direction from a point to another normalized.</returns>
+    public static Vector2 GetUnitDirectionVector(Vector2 from, Vector2 to)
     {
-      return (To - From).normalized;
+      return (to - from).normalized;
     }
   }
 }
